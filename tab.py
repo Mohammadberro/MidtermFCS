144c4c3e980803e5b65adfa324a75d81ea09363e
Mohammadberro <ikerberro1@gmail.com>
2023-11-25 03:19:26 +0200

    Creating a dictionary for the tab

--- tab.py
+++ tab.py
@@ -2,4 +2,7 @@ class Tab:
     def __init__(self, title, url):
         self.title = title
         self.url = url
-
+        self.tabDict = {
+            "title": title,
+            "url": url
+        }
