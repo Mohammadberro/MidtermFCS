e57c8ece29616339201f8bcc6e87db52e5e77cd9
Mohammadberro <ikerberro1@gmail.com>
2023-11-26 18:12:39 +0200

    Shifting Function to a new class: Browser

---
+++ browser.py
@@ -0,0 +1,127 @@
+from tab import Tab
+import requests
+import json
+
+
+class Browser:
+    def __init__(self):
+        self.tabs = []
+        self.tabs_dict_list = []
+
+    def open_tab(self, Title, URL, isParent=True):
+        new_tab = Tab(Title, URL)
+        if isParent:
+            self.tabs.append(new_tab)
+            self.tabs_dict_list.append(new_tab)
+            print(f"Tab has been opened successfully")
+        return new_tab
+
+    def close_tab(self, index):
+        try:
+            index = int(index)
+            tab_title = self.tabs[index].title
+            self.tabs.pop(index)
+        except IndexError:
+            print(f"index '{index}' is out of range.")
+        except ValueError:
+            print(f"index should be an integer.")
+        else:
+            print(f"The tab: {tab_title} has been removed successfully.")
+
+    def switch_tab(self, index=-1):
+        desired_url = self.tabs[index].url
+        choice = input("View last tab? y/n\t").lower()
+        while choice != ("y" or "n"):
+            choice = input("Invalid choice. Please answer with 'y' or 'n' ")
+        if choice == "y":
+            pass
+        else:
+            try:
+                index = int(input("Please specify the index of the tab you want to view:\t"))
+            except ValueError:
+                print(f"index should be an integer. Try again.")
+                return
+        try:
+            response = requests.get(desired_url)
+        except IndexError:
+            print(f"index '{index}' is out of range. Try again")
+            return
+        except requests.exceptions.MissingSchema:
+            print(f"The url: {desired_url} is invalid."
+                  f"Make sure you added a URL Scheme (ex: https://). ")
+            if input(f"Do you want to change the url : {desired_url} y/n") == "y":
+                self.tabs[index].url = input(f"Enter the new url for {self.tabs[index].title}")
+                self.switch_tab(index)
+            return
+        except requests.exceptions.ConnectionError:
+            print(f"The url: {desired_url} is invalid.")
+            if input(f"Do you want to change the url : {desired_url} y/n") == "y":
+                self.tabs[index].url = input(f"Enter the new url for {self.tabs[index].title}")
+                self.switch_tab(index)
+            return
+        website_html = response.text
+        self.tabs[index].web_source = website_html
+        print(website_html)
+
+    def display_all_tabs(self):
+        for n in range(0, len(self.tabs)):
+            print(f"{n + 1}.\t{self.tabs[n].title}")
+            if self.tabs[n].nested_tabs:
+                for i in range(0, len(self.tabs[n].nested_tabs)):
+                    print(f"\t{n + 1}.{i + 1}\t{self.tabs[n].nested_tabs[i].title}")
+
+    def open_nested_tab(self, indicator):
+        print("To add a nested tab, Enter:")
+        Title = input("Title:\t").title()
+        URL = input("Enter:\nURL:\t")
+        sub_tab = self.open_tab(Title, URL, False)
+        if indicator.isdigit():
+            parent_tab = self.tabs[int(indicator)]
+            try:
+                parent_tab.nested_tabs.append(sub_tab)
+                parent_tab.nested_tabs_dict.append(sub_tab.dict)
+            except IndexError:
+                print(f"index '{indicator}' is out of range.")
+                return
+            else:
+                print(f"{sub_tab.title} has been opened inside {parent_tab.title}")
+        else:
+            for tab in self.tabs:
+                if tab.title == indicator:
+                    tab.nested_tabs.append(sub_tab)
+                    print(f"{sub_tab.title} has been opened inside {tab.title}")
+                    break
+                else:
+                    print(f"There is no tab with the title {indicator}")
+
+    def sort_all_opened_tabs(self):
+        Titles = self.get_all_titles()
+        boarder = 0
+        while boarder < (len(Titles) - 1):
+            minIndex = boarder
+            for i in range(boarder + 1, len(Titles)):
+                if Titles[minIndex].lower() > Titles[i].lower():
+                    minIndex = i
+            temp = Titles[boarder]
+            Titles[boarder] = Titles[minIndex]
+            Titles[minIndex] = temp
+            boarder += 1
+        print(Titles)
+
+    def get_all_titles(self):
+        Titles = []
+        for tab in self.tabs:
+            Titles.append(tab.title)
+            for sub_tab in tab.nested_tabs:
+                Titles.append(sub_tab.title)
+        return Titles
+
+    def save_tabs(self, directory):
+        string = json.dumps(self.tabs_dict_list)
+        with open(directory, 'w') as file:
+            file.write(string)
+
+    def import_tabs(self, directory):
+        with open(directory, 'r') as f:
+            lines = f.read()
+            self.tabs_dict_list = json.loads(lines)
--- main.py
+++ main.py
@@ -1,129 +1,6 @@
-from tab import Tab
-import requests
-import json
+from browser import Browser
 
-tabs = []
-tabs_dict_list = []
-
-
-def open_tab(Title, URL, isParent=True):
-    new_tab = Tab(Title, URL)
-    if isParent:
-        tabs.append(new_tab)
-        tabs_dict_list.append(new_tab)
-    print(f"Tab has been opened successfully")
-    return new_tab
-
-
-def close_tab(index):
-    try:
-        index = int(index)
-        tab_title = tabs[index].title
-        tabs.pop(index)
-    except IndexError:
-        print(f"index '{index}' is out of range.")
-    except ValueError:
-        print(f"index should be an integer.")
-    else:
-        print(f"The tab: {tab_title} has been removed successfully.")
-
-
-def switch_tab(index=-1):
-    desired_url = tabs[index].url
-    choice = input("View last tab? y/n")
-    if choice == "y":
-        pass
-    else:
-        try:
-            index = int(input("Please specify the index of the tab you want to view:\t"))
-        except ValueError:
-            print(f"index should be an integer.")
-            return
-    try:
-        response = requests.get(desired_url)
-    except IndexError:
-        print(f"index '{index}' is out of range.")
-        return
-    except requests.exceptions.MissingSchema:
-        print(f"The url: {desired_url} is invalid."
-              f"Make sure you added a URL Scheme (ex: https://). ")
-        if input(f"Do you want to change the url : {requests.get(desired_url)} y/n") == "y":
-            tabs[index].url = input(f"Enter the new url for {requests.get(tabs[index].title)}")
-            switch_tab(index)
-        return
-    website_html = response.text
-    tabs[index].web_source = website_html
-    print(website_html)
-
-
-def display_all_tabs():
-    for n in range(0, len(tabs)):
-        print(f"{n + 1}.\t{tabs[n].title}")
-        if tabs[n].nested_tabs:
-            for i in range(0, len(tabs[n].nested_tabs)):
-                print(f"\t{n + 1}.{i + 1}\t{tabs[n].nested_tabs[i].title}")
-
-
-def open_nested_tab(indicator):
-    print("To add a nested tab, Enter:")
-    Title = input("Title:\t").title()
-    URL = input("Enter:\nURL:\t")
-    sub_tab = open_tab(Title, URL, False)
-    if indicator.isdigit():
-        parent_tab = tabs[int(indicator)]
-        try:
-            parent_tab.nested_tabs.append(sub_tab)
-            parent_tab.nested_tabs_dict.append(sub_tab.dict)
-        except IndexError:
-            print(f"index '{indicator}' is out of range.")
-            return
-        else:
-            print(f"{sub_tab.title} has been opened inside {parent_tab.title}")
-    else:
-        for tab in tabs:
-            if tab.title == indicator:
-                tab.nested_tabs.append(sub_tab)
-                print(f"{sub_tab.title} has been opened inside {tab.title}")
-                break
-            else:
-                print(f"There is no tab with the title {indicator}")
-
-
-def sort_all_opened_tabs(tab_list):
-    Titles = get_all_titles(tab_list)
-    boarder = 0
-    while boarder < (len(Titles) - 1):
-        minIndex = boarder
-        for i in range(boarder + 1, len(Titles)):
-            if Titles[minIndex].lower() > Titles[i].lower():
-                minIndex = i
-        temp = Titles[boarder]
-        Titles[boarder] = Titles[minIndex]
-        Titles[minIndex] = temp
-        boarder += 1
-    print(Titles)
-
-
-def get_all_titles(tab_list):
-    Titles = []
-    for tab in tab_list:
-        Titles.append(tab.title)
-        for sub_tab in tab.nested_tabs:
-            Titles.append(sub_tab.title)
-    return Titles
-
-
-def save_tabs(directory):
-    string = json.dumps(tabs_dict_list)
-    with open(directory, 'w') as file:
-        file.write(string)
-
-
-def import_tabs(directory):
-    with open(directory, 'r') as f:
-        lines = f.read()
-        dict_list = json.loads(lines)
-    return dict_list
+browser = Browser()
 
 
 def mainProgram():
@@ -148,25 +25,28 @@ def mainProgram():
                     print("To add a tab, Enter:")
                     Title = input("Title:\t").title()
                     URL = input("Enter:\nURL:\t")
-                    open_tab(Title, URL)
+                    browser.open_tab(Title, URL)
                 if option == 2:
                     index = input("Enter the index of the tab you wish to close:\t")
-                    close_tab(index)
+                    browser.close_tab(index)
                 if option == 3:
-                    switch_tab()
+                    browser.switch_tab()
                 if option == 4:
-                    display_all_tabs()
+                    browser.display_all_tabs()
                 if option == 5:
                     indicator = input("Indicate the Title or index Parent Tab:\t")
-                    open_nested_tab(indicator)
+                    browser.open_nested_tab(indicator)
                 if option == 6:
-                    sort_all_opened_tabs(tabs)
+                    browser.sort_all_opened_tabs()
                 if option == 7:
                     directory = input("Please specify a directory to save to")
-                    save_tabs(directory)
+                    browser.save_tabs(directory)
                 if option == 8:
                     directory = input("Please specify a directory to load from")
-                    import_tabs(directory)
+                    browser.import_tabs(directory)
+                if option == 9:
+                    print("Exiting the program...")
+                    break
             else:
                 print("Choice is invalid. Try again.")
 
